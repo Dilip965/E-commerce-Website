d4cd672a864ecd7a771fe2bddc31c2c3893389b7
Dilip965 <nandiwaledilip@gmail.com>
2025-02-14 15:48:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './component/header/header'
+function App() {
+ 
+
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
